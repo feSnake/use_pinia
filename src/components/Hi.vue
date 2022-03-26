89ac5aa6291d024b8994ec52b63d35c52b3138bf
Chai <chaichaichia@163.com>
2022-03-27 01:17:36 +0800

    fix: storeToRefs 结构赋值后 仍旧生效

--- src/components/Hi.vue
+++ src/components/Hi.vue
@@ -4,12 +4,19 @@
     <div class="name">Chai</div>
     <div class="hi">{{ store.hi }}</div>
     <div class="count">{{ store.count }}</div>
+
+    <hr />
+
+    <div class="hi">{{ hi }}</div>
+    <div class="count">{{ count }}</div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { storeToRefs } from "pinia";
 import { mainStore } from "../store";
 const store = mainStore();
+const { hi, count } = storeToRefs(store);
 </script>
 
 <style lang="scss" scoped>
