580d5ea634477b7290344c37e13c19b4221d675b
Chai <chaichaichia@163.com>
2022-03-27 00:49:57 +0800

    add: Hi.vue

---
+++ src/components/Hi.vue
@@ -0,0 +1,10 @@
+<!-- Hi -->
+<template>
+  <div class=""></div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/store/index.ts
@@ -0,0 +1,17 @@
+/*
+定义状态容器(仓库)
+修改容器(仓库)中的 state
+仓库中的 action 的使用
+*/
+
+import { defineStore } from "pinia";
+
+export const mainStore = defineStore("main", {
+  state: () => {
+    return {
+      hi: "hi chai!",
+    };
+  },
+  getters: {},
+  actions: {},
+});
